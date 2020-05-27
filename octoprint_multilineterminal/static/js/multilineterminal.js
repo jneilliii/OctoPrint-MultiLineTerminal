a663ba4b992e2ca39471892f9a49051dcb4362b3
jneilliii <jneilliii+github@gmail.com>
2020-05-27 19:51:15 -0400

    0.1.3
    added shift+up and shift+down to navigate command history
    removed history buttons

--- octoprint_multilineterminal/static/js/multilineterminal.js
+++ octoprint_multilineterminal/static/js/multilineterminal.js
@@ -45,10 +45,10 @@ $(function() {
 			var commandsToSend = commandsToSend.filter(function(array_val) {
 					return Boolean(array_val) === true;
 				});
-console.log(commandsToSend);
+
 			OctoPrint.control.sendGcode(commandsToSend)
 				.done(function() {
-					self.terminalViewModel.cmdHistory.push(command);
+					self.terminalViewModel.cmdHistory.push(command.replace(/\n$/,''));
 					self.terminalViewModel.cmdHistory.slice(-300); // just to set a sane limit to how many manually entered commands will be saved...
 					self.terminalViewModel.cmdHistoryIdx = self.terminalViewModel.cmdHistory.length;
 					self.terminalViewModel.command("");
@@ -69,16 +69,30 @@ console.log(commandsToSend);
 			}
 		}
 
+		self.terminalViewModel.handleKeyDownMultiLine = function(e){
+			if (event.shiftKey && event.keyCode === 38) {
+				self.terminalViewModel.getHistoryMultiLine(1);
+				return false;
+			}
+			if (event.shiftKey && event.keyCode === 40) {
+				self.terminalViewModel.getHistoryMultiLine(-1);
+				return false;
+			}
+			// do not prevent default action
+			return true;
+		}
+
 		self.terminalViewModel.handleKeyUpMultiLine = function(e){
 			if (event.shiftKey && event.keyCode === 13) {
 				$('#terminal-send').trigger('click');
+				return false;
 			}
 			// do not prevent default action
 			return true;
 		};
 
 		self.onStartup = function(){
-			$('#terminal-command').replaceWith('<span class="add-on" id="multilinehistorybuttons"><a class="icon icon-arrow-up" data-bind="click: function(){getHistoryMultiLine(1);}, enable: isOperational() && loginState.isUser()"></a><a class="icon icon-arrow-down" data-bind="click: function(){getHistoryMultiLine(-1);}, enable: isOperational() && loginState.isUser()"></a></span><textarea rows="4" class="input input-block-level" id="terminal-command" data-bind="textInput: command, event: { keyup: function(d,e) { return handleKeyUpMultiLine(e); } }, enable: isOperational() && loginState.isUser()"/>').parent('div').addClass('input prepend');
+			$('#terminal-command').replaceWith('<textarea rows="4" class="input input-block-level" id="terminal-command" data-bind="textInput: command, event: { keyup: function(d,e) { return handleKeyUpMultiLine(e); }, keydown: function(d,e) { return handleKeyDownMultiLine(e); } }, enable: isOperational() && loginState.isUser()"/>').parent('div').addClass('input prepend');
 			$('#terminal-send').attr('data-bind','click: sendCommandMultiLine, enable: isOperational() && loginState.isUser()');
 		}
 	}
